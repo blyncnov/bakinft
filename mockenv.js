9a79ff2104c82aa9d6ab1287b7452238b44262db
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-06-18 10:31:31 +0100

    Here is baki NFT v2

---
+++ mockenv.js
@@ -0,0 +1,24 @@
+# Rinkeby Endpoint API
+https://rinkeby.infura.io/v3/e5e3873c019343c9b54cec439aef2655
+
+
+# Edit your Private key below
+
+PRIVATE_KEY = 99c84cdffb99d749dceca5aa6c7424db7edc086f06d59083cce73f4373285504
+
+PROJECT_ID = e5e3873c019343c9b54cec439aef2655
+
+ETHERSCAN_API_KEY = ADI33GVI2BGX4RTQYNTGFHPZGQWTZ7VGR2
+
+
+
+# Variable for the User Interface
+
+CONTRACT_ADDRESS = Contract address
+MAX_MINT = Max mint count
+BASE_URL = Token base uri
+
+
+# My test variables
+MY_TEST = "I love this Baki"
+
